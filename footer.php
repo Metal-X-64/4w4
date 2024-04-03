f50cf715d49aceadf64fb77f31fe93a8c01c0a34
Samuel Giroux <s_giroux@icloud.com>
2024-04-03 09:52:38 -0400

    Amélioration de l'apparence du footer et ajout de liens dans le footer.

--- footer.php
+++ footer.php
@@ -7,9 +7,25 @@ Ce gabarit est appelé à la fin de chacun des modèles de thème
 
 <div id="footer" class="global">
         <footer>
-            <h3>Footer (h3)</h3>
-            <h5>À propos (h5)</h5>
-            <h6>Nous contacter (h6)</h6>
+            <div class="colones-footer">
+                <div>
+                    <h3>À propos</h3>
+                    <a href="#">Qui sommes nous?</a>
+                </div>
+                <div>
+                    <h3>Besoin d'aide?</h3>
+                    <a href="#">Aide et assistance</a>
+                    <a href="#">Recherche</a>
+                </div>
+                <div>
+                    <h3>Nous trouver</h3>
+                    <a href="#">Facebook</a>
+                    <a href="#">Twitter</a>
+                    <a href="#">YouTube</a>
+                    <a href="#">Instagram</a>
+                </div>
+            </div>
+            
             <a href="#">Retour vers le haut</a>
         </footer>
     </div>
