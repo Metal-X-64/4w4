ff46382c9a777422840e14082f68309687f17a04
Samuel Giroux <s_giroux@icloud.com>
2024-04-03 11:05:46 -0400

    Ajout d'un menu dans le footer

--- footer.php
+++ footer.php
@@ -8,6 +8,7 @@ Ce gabarit est appelé à la fin de chacun des modèles de thème
 <div id="footer" class="global">
         <footer>
             <div class="colones-footer">
+                
                 <div>
                     <h3>À propos</h3>
                     <a href="#">Qui sommes nous?</a>
@@ -15,16 +16,27 @@ Ce gabarit est appelé à la fin de chacun des modèles de thème
                 <div>
                     <h3>Besoin d'aide?</h3>
                     <a href="#">Aide et assistance</a>
-                    <a href="#">Recherche</a>
+                    <a href="https://www.google.com/">Recherche</a>
+                </div>
+                <div class="flexbox__sites">
+                
                 </div>
                 <div>
                     <h3>Nous trouver</h3>
-                    <a href="#">Facebook</a>
-                    <a href="#">Twitter</a>
-                    <a href="#">YouTube</a>
-                    <a href="#">Instagram</a>
+                    <a href="https://www.facebook.com/">Facebook</a>
+                    <a href="https://www.twitter.com/">Twitter</a>
+                    <a href="https://www.youtube.com/">YouTube</a>
+                    <a href="https://www.instagram.com/">Instagram</a>
                 </div>
+
             </div>
+            <section class="flexbox__elm">
+                <h3>Nos partenaires</h3>
+            <?php 
+                    wp_nav_menu(array(
+                        "menu" => "footer",
+                        "container" => "nav")); ?>
+            </section>
             
             <a href="#">Retour vers le haut</a>
         </footer>
