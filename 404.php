140d00751f317d3a20aac1cbaa72e7c18b6820d2
Samuel Giroux <s_giroux@icloud.com>
2024-04-11 11:38:33 -0400

    Adaptation des liens des catégories, du bouton, de la recherche et des textes aux différentes résolutions d'écrans.

--- 404.php
+++ 404.php
@@ -3,15 +3,27 @@
 <!-- ////////////////// Contenu principal /////////////////// -->
     <div id="erreur_404" class="global clr-agencement-secondaire">
         <div id="message_erreur">
-            <h2>Erreur 404</h2>
-            <h4>Vous essayez d'accéder à une page qui n'existent pas</h4>
-            <h4>Pour revenir à la page d'accueil, cliquez sur le lien suivant:</h4>
+            <div>
+                <h2>Erreur 404</h2>
+                <h4>Vous essayez d'accéder à une page qui n'existent pas.</h4>
+                <h4>Pour revenir à la page d'accueil, cliquez sur le lien suivant:</h4>
+            </div>
             <br>
-            <a class="bouton_erreur" href="<?php echo get_bloginfo('url'); ?>"><?php echo get_bloginfo('name'); ?></a>
+            <div>
+                <a class="bouton_erreur" href="<?php echo get_bloginfo('url'); ?>"><?php echo get_bloginfo('name'); ?></a>
+            </div>
             <?php get_search_form(); ?>
-            <?php wp_nav_menu(array(
-                "menu" => "principal",
-                "container" => "nav")); ?>
+            <br>
+            <div id="categories_erreur">
+                <a href="/4w4/category/aventure/">Aventure</a>
+                <a href="/4w4/category/croisiere/">Croisière</a>
+                <a href="/4w4/category/culturel/">Culturel</a>
+                <a href="/4w4/category/pleine-nature/">Pleine nature</a>
+                <a href="/4w4/category/populaire/">Populaire</a>
+                <a href="/4w4/category/repos/">Repos</a>
+                <a href="/4w4/category/sport/">Sport</a>
+                <a href="/4w4/category/zen/">Zen</a>
+            </div>
         </div>
         <div>
             <img class="erreur-image" src="https://gftnth00.mywhc.ca/tim16/wp-content/uploads/2024/04/logo-voyage2.png" alt="logo" width="400px" height="400px">
